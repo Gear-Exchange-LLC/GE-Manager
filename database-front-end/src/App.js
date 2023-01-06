57b428a2f9397bfab461849f3275f7737c1dad6c
DevGamer9991 <pathornton114@gmail.com>
2023-01-06 16:02:38 -0600

    Update

--- database-front-end/src/App.js
+++ database-front-end/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const [brand, setBrand] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Wrapper">
+        <div className="InputItem">
+          <p>Brand:</p>
+          <input type="text" onChange={(event) => setBrand(event.target.value)}/>
+        </div>
+        <div className="InputItem">
+          <p>Model:</p>
+          <input type="text" onChange={(event) => setBrand(event.target.value)}/>
+        </div>
+        <div className="InputItem">
+          <p>SKU:</p>
+          <input type="text" onChange={(event) => setBrand(event.target.value)}/>
+        </div>
+        <div className="InputItem">
+          <p>Price:</p>
+          <input type="text" onChange={(event) => setBrand(event.target.value)}/>
+        </div>
+        <div className="InputItem">
+          <p>Condition:</p>
+          <input type="text" onChange={(event) => setBrand(event.target.value)}/>
+        </div>
+      </div>
     </div>
   );
 }
