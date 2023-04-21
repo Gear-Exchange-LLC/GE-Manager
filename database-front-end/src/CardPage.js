c1011d0d295dccff13207dfa096182f0169c0f9f
Parker Thornton <pathornton114@gmail.com>
2023-04-21 12:59:26 -0500

    Added Editing Ability

--- database-front-end/src/CardPage.js
+++ database-front-end/src/CardPage.js
@@ -21,6 +21,7 @@ function CardPage() {
     const [editLoading, setEditLoading] = React.useState(false);
     const [deleteLoading, setDeleteLoading] = React.useState(false);
     const [createListingLoading, setCreateListingLoading] = React.useState(false);
+    const [editListingLoading, setEditListingLoading] = React.useState(false);
     const [completeLoading, setCompleteLoading] = React.useState(false);
 
     const navigate = useNavigate()
@@ -35,6 +36,7 @@ function CardPage() {
             setData(item)
             setCompleteLoading(false);
             setCreateListingLoading(false);
+            setEditListingLoading(false);
             setDeleteLoading(false);
           }
         })
@@ -116,6 +118,13 @@ function CardPage() {
       await socket.emit("create-reverb", data)
     }
 
+    const editListing = async () => {
+      
+      setEditListingLoading(true);
+
+      navigate("/edit/" + data.transactionID)
+    }
+
     const markComplete = async () => {
       
       setCompleteLoading(true);
@@ -171,6 +180,18 @@ function CardPage() {
           </Box>
         </Box>
         <Stack direction="row" spacing={2} justifyContent="end" paddingRight={0} width="90%" marginBottom={2} marginLeft={"5%"}>
+          <Button color="primary" variant="outlined" sx={{ width: 200 }} disabled={createListingLoading || data.reverbCreated == true} onClick={() => createListing()}>Create Listing {createListingLoading && (
+                  <CircularProgress
+                    size={24}
+                    sx={{
+                      color: "green",
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      marginTop: '-12px',
+                      marginLeft: '-12px',
+                    }}
+                  />)}</Button>
           <Button color="primary" variant={ data.completed ? "outlined" : "contained"} sx={{ width: 200 }} disabled={completeLoading} onClick={() => markComplete()}>{ data.completed ? "Mark as UnComplete" : "Mark as Complete" }{deleteLoading && (
                 <CircularProgress
                   size={24}
@@ -263,7 +284,7 @@ function CardPage() {
                     marginLeft: '-12px',
                   }}
                 />)}</Button> */}
-          <Button color="primary" variant="outlined" sx={{ width: 200 }} disabled={createListingLoading || data.reverbCreated == true} onClick={() => createListing()}>Create Listing {createListingLoading && (
+          <Button color="primary" variant="outlined" sx={{ width: 200 }} disabled={editListingLoading} onClick={() => editListing()}>Edit {editListingLoading && (
                 <CircularProgress
                   size={24}
                   sx={{
