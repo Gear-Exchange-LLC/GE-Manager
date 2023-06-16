5369bee297fc356d168ca7b95e9921cc972b62e1
Parker Thornton <pathornton114@gmail.com>
2023-06-16 16:05:52 -0500

    Update SearchItemsPage.js

--- database-front-end/src/SearchItemsPage.js
+++ database-front-end/src/SearchItemsPage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import React, { useEffect, useContext } from "react"
 import { SocketContext } from "./context/SocketContext";
 
